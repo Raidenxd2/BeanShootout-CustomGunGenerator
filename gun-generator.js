ddc4f36d3cc9b040abe9f0532d0dae2cb33c0b16
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2024-04-06 19:39:11 -0400

    v1

---
+++ gun-generator.js
@@ -0,0 +1,33 @@
+function generate() {
+    // Get inputs
+    const gunNameInput = document.getElementById("gunNameInput");
+    const amountOfBulletsToShootInput = document.getElementById("amountOfBulletsToShootInput");
+    const bulletsThatAreUsedInput = document.getElementById("bulletsThatAreUsedInput");
+    const damageInput = document.getElementById("damageInput");
+    const holdToShootInput = document.getElementById("holdToShootInput");
+    const delayInput = document.getElementById("delayInput");
+    const imageInput = document.getElementById("imageInput");
+
+    // Create object with values
+    var obj = new Object();
+    obj.GunName = gunNameInput.value;
+    obj.AmountOfBulletsToShoot = amountOfBulletsToShootInput.value;
+    obj.BulletsThatAreUsed = bulletsThatAreUsedInput.value;
+    obj.Damage = damageInput.value;
+    obj.HoldToShoot = holdToShootInput.value;
+    obj.Delay = delayInput.value;
+
+    // Create JSON
+    var jsonString = JSON.stringify(obj);
+    
+    // Create Zip
+    var zip = new JSZip();
+
+    zip.file("gun.json", jsonString);
+    zip.file("image.png", imageInput.files[0])
+
+    zip.generateAsync({type: "blob"})
+    .then(function(content) {
+        saveAs(content, gunNameInput.value + ".zip");
+    });
+}
